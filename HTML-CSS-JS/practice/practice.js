0536324688b5ef0ca5fe1ffe9ad4b25d32ec1d59
unknown <roshananand2005@gmail.com>
2024-05-02 18:23:53 +0530

    learnt  mouse animation

--- HTML-CSS-JS/practice/practice.js
+++ HTML-CSS-JS/practice/practice.js
@@ -1,16 +1,19 @@
-const boxes = document.querySelectorAll('.boxes');
+const box = document.getElementById("container");
+let center;
+box.addEventListener("mousemove", (point) => {
+    const checkLeft = () => {
+        if (center - point.x < 255) return center - point.x;
+        else return 225;
+    }
+    const checkRight = () => {
+        if (point.x-center > 255) return 225;
+        else return point.x - center;
+    }
+    center = window.innerWidth / 2;
+    if (point.x > center) box.style.backgroundColor = `rgb( ${checkRight()},225, ${checkRight()})`;
+    else box.style.backgroundColor = `rgb(255, ${checkLeft()}, ${checkLeft()})`;
+})
 
-
-boxes.forEach((divEle)=>{
-    let imgEle;
-    divEle.addEventListener("mousemove", (point) => {
-        imgEle = divEle.firstElementChild;
-        imgEle.style.opacity = "1";
-        imgEle.style.left = `${point.x-50}px`;
-        imgEle.style.top = `${point.y-50}px`;
-    })
-    divEle.addEventListener("mouseleave", (point) => {
-        imgEle.style.opacity = "0";
-    })
-
+document.addEventListener("mousemove",()=>{
+    
 })
